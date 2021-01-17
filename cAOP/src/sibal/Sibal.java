790c1dd86bf1f253bfd42d5dc4af89f964559c1d
qwe5507 <70142711+qwe5507@users.noreply.github.com>
2021-01-17 15:40:55 +0900

    three commit

---
+++ cAOP/src/sibal/Sibal.java
@@ -0,0 +1,12 @@
+package sibal;
+
+import java.sql.SQLOutput;
+
+import javax.sql.rowset.serial.SQLOutputImpl;
+
+public class Sibal {
+	public static void main(String[] args) {
+		System.out.println("sibal");
+	}
+	
+}
